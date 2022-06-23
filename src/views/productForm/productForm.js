ba68975d4cd943638c82e5200d741125036d1946
hb912 <43055448+hb912@users.noreply.github.com>
2022-06-23 22:56:54 +0900

    (버그)productForm의 상품 제거 기능 다시 추가
    깃 오류로 날아간 상품 제거 기능 다시 추가

--- src/views/productForm/productForm.js
+++ src/views/productForm/productForm.js
@@ -1,5 +1,6 @@
 import * as Api from "/api.js";
 import { $ } from "/utils.js";
+import header from "/header.js";
 
 const URLSearch = new URLSearchParams(location.search);
 const id = URLSearch.get("product");
@@ -9,68 +10,70 @@ const detailDescriptionInput = document.getElementById(
   "detailDescriptionInput"
 );
 const imageInput = document.getElementById("imageInput");
-const inventoryInput = document.getElementById("inventoryInput");
 const submitButton = document.getElementById("submitButton");
 const fileNameSpan = document.getElementById("fileNameSpan");
 const priceInput = document.getElementById("priceInput");
 const categorySelectBox = document.getElementById("categorySelectBox");
-const btn = document.getElementById("imgbutton");
 const inventorySubmit = document.getElementById("inventorySubmit");
 const outputdata = document.getElementById("outputdata");
-const sizeInput = document.getElementById("sizeInput");
-const sizeList = document.querySelectorAll("sizeList");
-const categories = await Api.get("/api/category");
+const headerParent = $("body");
 let category = "";
-console.log(categories);
 
-categories.map((data) => {
-  const select = document.createElement("option");
-  select.value = data.name;
-  select.innerText = data.name;
-  categorySelectBox.appendChild(select);
-});
+addAllElements();
 
-categorySelectBox.addEventListener("change", () => {
-  category = categorySelectBox.value;
-  console.log(category);
-});
-submitButton.addEventListener("click", submitEvent);
-imageInput.addEventListener("change", imageNameChange);
-inventorySubmit.addEventListener("click", sizeSubmitEvent);
-console.log();
+async function addAllElements() {
+  header(headerParent);
+  const categories = await Api.get("/api/category");
+  categories.map((data) => {
+    const select = document.createElement("option");
+    select.value = data.name;
+    select.innerText = data.name;
+    categorySelectBox.appendChild(select);
+  });
 
-if (id) {
-  try {
-    const product = await Api.get("/api/product/detail", id);
-    $("#titleInput").value = product.name;
-    $("#categorySelectBox").value = product.category;
-    category = product.category;
-    $("#manufacturerInput").value = product.brand;
-    $("#detailDescriptionInput").value = product.content;
-    product.size.map((data) => {
-      const list = document.createElement("li");
-      const liEl = `사이즈<input class='sizeField' type="text" value=${data.name}>&nbsp&nbsp&nbsp&nbsp&nbsp수량<input type="Number" class='quantityField' value=${data.stock}>`;
-      list.innerHTML = liEl;
-      list.classList.add("sizeList");
-      outputdata.appendChild(list);
-    });
-    priceInput.value = product.price;
-  } catch (e) {
-    console.error(e.stack);
-    swal(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${e.message}`);
+  categorySelectBox.addEventListener("change", () => {
+    category = categorySelectBox.value;
+  });
+  submitButton.addEventListener("click", submitEvent);
+  imageInput.addEventListener("change", imageNameChange);
+  inventorySubmit.addEventListener("click", sizeSubmitEvent);
+
+  if (id) {
+    try {
+      const product = await Api.get("/api/product/detail", id);
+      $("#titleInput").value = product.name;
+      $("#categorySelectBox").value = product.category;
+      category = product.category;
+      $("#manufacturerInput").value = product.brand;
+      $("#detailDescriptionInput").value = product.content;
+      product.size.map((data) => {
+        const list = document.createElement("li");
+        const liEl = `사이즈<input class='input sizeField' type="text" value=${data.name}>&nbsp&nbsp&nbsp&nbsp&nbsp수량<input type="Number" class='input quantityField' value=${data.stock}>`;
+        list.innerHTML = liEl;
+        list.classList.add("sizeList");
+        outputdata.appendChild(list);
+      });
+      priceInput.value = product.price;
+    } catch (e) {
+      console.error(e.stack);
+      swal(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${e.message}`);
+    }
+    submitButton.innerText = "제품 수정하기";
+    const deleteDiv = $("div.mt-5");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "button is-danger is-fullwidth";
+    deleteBtn.id = "deleteBtn";
+    deleteBtn.innerText = "제품 삭제하기";
+    deleteBtn.addEventListener("click", deleteProduct);
+    deleteDiv.appendChild(deleteBtn);
   }
-  submitButton.innerText = "제품 수정하기";
 }
 
 function sizeSubmitEvent(e) {
   e.preventDefault();
 
-  // const result1 = size.push({
-  //   name: sizeInput.value,
-  //   stock: Number(inventoryInput.value),
-  // });
   const list = document.createElement("li");
-  const liEl = `사이즈<input class='sizeField' type="text"/>&nbsp&nbsp&nbsp&nbsp&nbsp수량<input type="Number" class='quantityField'/>`;
+  const liEl = `사이즈<input class='input sizeField' type="text"/>&nbsp&nbsp&nbsp&nbsp&nbsp수량<input type="Number" class='input quantityField'/>`;
   list.innerHTML = liEl;
   list.classList.add("sizeList");
   outputdata.appendChild(list);
@@ -80,18 +83,46 @@ function imageNameChange(e) {
   fileNameSpan.innerHTML = e.target.value;
 }
 
+async function deleteProduct(e) {
+  e.preventDefault();
+  try {
+    await Api.delete("/api/product", id);
+    swal("삭제되었습니다").then(() => (location.href = "/inventory"));
+    return;
+  } catch (e) {
+    console.error(e.stack);
+    swal(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${e.message}`);
+  }
+}
+
 async function submitEvent(e) {
   e.preventDefault();
   const sizeFields = document.querySelectorAll(".sizeField");
   const quantityFields = document.querySelectorAll(".quantityField");
   const size = [];
   for (let i = 0; i < sizeFields.length; i++) {
-    size.push({ name: sizeFields[i].value, stock: quantityFields[i].value });
+    if (sizeFields[i].value !== "" && Number(quantityFields[i].value) > 0) {
+      size.push({
+        name: sizeFields[i].value,
+        stock: Number(quantityFields[i].value),
+      });
+    }
   }
   const image = imageInput.files[0];
   let formdata = new FormData();
   if (image) {
     formdata.append("image-file", image);
+  }
+  if (
+    !titleInput.value ||
+    !priceInput.value ||
+    !detailDescriptionInput.value ||
+    !manufacturerInput.value ||
+    !category ||
+    size.length < 1
+  ) {
+    swal("빈 칸이 있습니다");
+    return;
   }
   formdata.append("name", titleInput.value);
   formdata.append("price", priceInput.value);
@@ -99,18 +130,14 @@ async function submitEvent(e) {
   formdata.append("brand", manufacturerInput.value);
   formdata.append("category", category);
   formdata.append("size", JSON.stringify(size));
-  // formdata.append('',sizeInput.value)
-  // formdata.append('',inventoryInput.value)
-  // formdata.append('productQuantity',inventoryInput.value);
-  // console.log(image);
-  //   console.log(JSON.stringify(formdata));
+
   try {
     if (id) {
       const result = await Api.formDataPatch("/api/product", formdata, id);
       swal("수정완료").then(() => (location.href = "/inventory"));
       return;
     }
-    const result = await Api.formDataPost("/api/product/uplodimg", formdata);
+    await Api.formDataPost("/api/product/uplodimg", formdata);
     swal("업로드 완료").then(() => (location.href = "/inventory"));
   } catch (e) {
     console.error(e.stack);
